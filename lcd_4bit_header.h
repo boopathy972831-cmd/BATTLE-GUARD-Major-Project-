f20acaa5b5b9d5eca5eb5a435e709a917e41c1c5
boopathy972831-cmd <boopathy972831@gmail.com>
2026-06-17 12:07:33 +0530

    Add files via upload

---
+++ lcd_4bit_header.h
@@ -0,0 +1,151 @@
+#include<lpc21xx.h>
+#include"delay_header.h"
+
+#define lcd 0xf0
+
+#define rs 1<<8
+
+#define E 1<<9
+
+
+void lcd_init(void);
+
+void lcd_command(unsigned char);
+
+void lcd_data(unsigned char);
+void lcd_str(unsigned char*);
+
+void lcd_str(unsigned char*c)
+{
+  while(*c)
+  lcd_data(*c++);
+  }
+
+void lcd_init(void)
+
+{
+
+IODIR0=lcd|rs|E;
+
+delay_milliseconds(20);
+
+IOCLR0=rs;
+IOCLR0=lcd;
+
+IOSET0=0x30;
+
+IOSET0=E;
+
+delay_milliseconds(5);
+
+IOCLR0=E;
+
+
+IOCLR0=lcd;
+
+IOSET0=0x30;
+
+IOSET0=E;
+
+delay_milliseconds(1);
+
+IOCLR0=E;
+
+
+IOCLR0=lcd;
+
+IOSET0=0x30;
+
+IOSET0=E;
+
+delay_milliseconds(1);
+
+IOCLR0=E;
+
+
+IOCLR0=lcd;
+
+IOSET0=0x20;
+
+IOSET0=E;
+
+delay_milliseconds(1);
+
+IOCLR0=E;
+
+
+lcd_command(0x01);//to clear the display
+
+lcd_command(0x02);//to place the cursor in home position
+
+lcd_command(0x0c);//display ON cursor OFF
+
+lcd_command(0x28);//8 bit interfacing mode with both lines 
+
+}
+
+void lcd_command(unsigned char cmd)
+
+{ //upper nibble
+
+IOCLR0=lcd;//to clear data pins
+
+IOSET0=(cmd&0xf0);//assigning cmd byte to datapins
+
+IOCLR0=rs;//rs=o,to select command register
+
+IOSET0=E;
+
+delay_milliseconds(2);
+
+IOCLR0=E;
+
+//lower nibble
+
+IOCLR0=lcd;//to clear data pins
+
+IOSET0=((cmd<<4)&0xf0);//assigning cmd byte to datapins
+
+IOCLR0=rs;//rs=o,to select command register
+
+IOSET0=E;
+
+delay_milliseconds(2);
+
+IOCLR0=E;
+
+}
+
+void lcd_data(unsigned char data)
+
+{
+
+// upper nibble
+
+IOCLR0=lcd;//to clear data pins
+
+IOSET0=(data&0xf0);//assigning cmd byte to datapins
+
+IOSET0=rs;//rs=o,to select command register
+
+IOSET0=E;
+
+delay_milliseconds(2);
+
+IOCLR0=E;
+
+
+// lower nibble
+
+IOCLR0=lcd;//to clear data pins
+
+IOSET0=((data<<4)&0xf0);//assigning cmd byte to datapins
+
+IOSET0=rs;//rs=o,to select command register
+
+IOSET0=E;
+
+delay_milliseconds(2);
+IOCLR0=E;
+}
+
